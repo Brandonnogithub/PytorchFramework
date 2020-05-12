2b8a8e31dc2a8d8e1497afda249698363d21fc0a
Brandonnogithub <2275135452@qq.com>
2020-05-12 23:16:29 +0800

    1

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from test1.test1 import test1
 if __name__ == "__main__":
     test1()
     a = sys.modules
-    print(a.keys())
+    # print(a.keys())
+    print(sys.path)
--- test1/__init__.py
+++ test1/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append("..")
+# sys.path.append("..")
 
 # __all__ = ["test3"]
 # print(__all__)
